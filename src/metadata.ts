3de30300023e1964b04ad7e3668971447f328cc4
Kelvin Jin <kelvinjin@google.com>
2017-08-11 16:36:26 -0700

    define metadata interface

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,3 +1,14 @@
-export class Metadata {
-  
+export type MetadataValue = string | Buffer;
+
+export interface MetadataObject {
+  [propName: string]: Array<MetadataValue>;
+}
+
+export interface Metadata {
+  set: (key: string, value: MetadataValue) => void;
+  add: (key: string, value: MetadataValue) => void;
+  remove: (key: string) => void;
+  get: (key: string) => Array<MetadataValue>;
+  getMap: () => MetadataObject;
+  clone: () => Metadata;
 }
