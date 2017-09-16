5513e9aab15b1d01cb88af87116998fe404b1f8b
Matt Kwong <mattkwong@google.com>
2017-09-15 17:54:21 -0700

    Change == to = for shell support

--- run-tests.sh
+++ run-tests.sh
@@ -22,7 +22,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | b
 set -e
 cd $(dirname $0)
 
-if [ "x$node_versions" == "x" ] ; then
+if [ "x$node_versions" = "x" ] ; then
   node_versions="6 7 8"
 fi
 
