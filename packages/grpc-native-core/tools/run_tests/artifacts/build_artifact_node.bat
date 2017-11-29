087d67441bdbb89cc71bbd097fb5c5a2e809da5e
murgatroid99 <mlumish@google.com>
2017-11-29 13:15:34 -0800

    Add alpine build script and Dockerfile, improve existing build scripts

--- packages/grpc-native-core/tools/run_tests/artifacts/build_artifact_node.bat
+++ packages/grpc-native-core/tools/run_tests/artifacts/build_artifact_node.bat
@@ -12,6 +12,8 @@
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 
+set arch_list=ia32 x64
+
 set node_versions=4.0.0 5.0.0 6.0.0 7.0.0 8.0.0 9.0.0
 
 set electron_versions=1.0.0 1.1.0 1.2.0 1.3.0 1.4.0 1.5.0 1.6.0 1.7.0
@@ -24,21 +26,23 @@ call npm update || goto :error
 
 mkdir -p %ARTIFACTS_OUT%
 
-for %%v in (%node_versions%) do (
-  call .\node_modules\.bin\node-pre-gyp.cmd configure build --target=%%v --target_arch=%1
+for %%a in (%arch_list%) do (
+  for %%v in (%node_versions%) do (
+    call .\node_modules\.bin\node-pre-gyp.cmd configure build --target=%%v --target_arch=%%a
 
-@rem Try again after removing openssl headers
-  rmdir "%USERPROFILE%\.node-gyp\%%v\include\node\openssl" /S /Q
-  rmdir "%USERPROFILE%\.node-gyp\iojs-%%v\include\node\openssl" /S /Q
-  call .\node_modules\.bin\node-pre-gyp.cmd build package --target=%%v --target_arch=%1 || goto :error
+  @rem Try again after removing openssl headers
+    rmdir "%USERPROFILE%\.node-gyp\%%v\include\node\openssl" /S /Q
+    rmdir "%USERPROFILE%\.node-gyp\iojs-%%v\include\node\openssl" /S /Q
+    call .\node_modules\.bin\node-pre-gyp.cmd build package --target=%%v --target_arch=%%a || goto :error
 
-  xcopy /Y /I /S build\stage\* %ARTIFACTS_OUT%\ || goto :error
-)
+    xcopy /Y /I /S build\stage\* %ARTIFACTS_OUT%\ || goto :error
+  )
 
-for %%v in (%electron_versions%) do (
-  cmd /V /C "set "HOME=%USERPROFILE%\electron-gyp" && call .\node_modules\.bin\node-pre-gyp.cmd configure rebuild package --runtime=electron --target=%%v --target_arch=%1 --disturl=https://atom.io/download/electron" || goto :error
+  for %%v in (%electron_versions%) do (
+    cmd /V /C "set "HOME=%USERPROFILE%\electron-gyp" && call .\node_modules\.bin\node-pre-gyp.cmd configure rebuild package --runtime=electron --target=%%v --target_arch=%%a --disturl=https://atom.io/download/electron" || goto :error
 
-  xcopy /Y /I /S build\stage\* %ARTIFACTS_OUT%\ || goto :error
+    xcopy /Y /I /S build\stage\* %ARTIFACTS_OUT%\ || goto :error
+  )
 )
 if %errorlevel% neq 0 exit /b %errorlevel%
 
--- packages/grpc-native-core/tools/run_tests/artifacts/build_artifact_node.sh
+++ packages/grpc-native-core/tools/run_tests/artifacts/build_artifact_node.sh
@@ -13,12 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-NODE_TARGET_ARCH=$1
-NODE_TARGET_OS=$2
-source ~/.nvm/nvm.sh
-
-nvm use 8
-set -ex
+NODE_ALPINE_BUILD=$1
 
 umask 022
 
@@ -30,24 +25,25 @@ mkdir -p "${ARTIFACTS_OUT}"
 
 npm update
 
+arch_list=( ia32 x64 )
+
 node_versions=( 4.0.0 5.0.0 6.0.0 7.0.0 8.0.0 9.0.0 )
 
 electron_versions=( 1.0.0 1.1.0 1.2.0 1.3.0 1.4.0 1.5.0 1.6.0 1.7.0 )
 
-for version in ${node_versions[@]}
+for arch in ${arch_list[@]}
 do
-  ./node_modules/.bin/node-pre-gyp configure rebuild package --target=$version --target_arch=$NODE_TARGET_ARCH --grpc_alpine=true
-  cp -r build/stage/* "${ARTIFACTS_OUT}"/
-  if [ "$NODE_TARGET_ARCH" == 'x64' ] && [ "$NODE_TARGET_OS" == 'linux' ]
-  then
-    # Cross compile for ARM on x64
-    CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ LD=arm-linux-gnueabihf-g++ ./node_modules/.bin/node-pre-gyp configure rebuild package testpackage --target=$version --target_arch=arm
+  for version in ${node_versions[@]}
+  do
+    ./node_modules/.bin/node-pre-gyp configure rebuild package --target=$version --target_arch=$arch --grpc_alpine=$NODE_ALPINE_BUILD
     cp -r build/stage/* "${ARTIFACTS_OUT}"/
-  fi
+  done
+
+  for version in ${electron_versions[@]}
+  do
+    HOME=~/.electron-gyp ./node_modules/.bin/node-pre-gyp configure rebuild package --runtime=electron --target=$version --target_arch=$arch --disturl=https://atom.io/download/electron
+    cp -r build/stage/* "${ARTIFACTS_OUT}"/
+  done
 done
 
-for version in ${electron_versions[@]}
-do
-  HOME=~/.electron-gyp ./node_modules/.bin/node-pre-gyp configure rebuild package --runtime=electron --target=$version --target_arch=$NODE_TARGET_ARCH --disturl=https://atom.io/download/electron
-  cp -r build/stage/* "${ARTIFACTS_OUT}"/
-done
+rm -rf build || true
