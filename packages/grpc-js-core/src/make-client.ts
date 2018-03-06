a3dc724d3974f045821a04b2382d04272975175f
Kelvin Jin <kelvinjin@google.com>
2018-03-06 15:06:25 -0800

    address comment and use camelCase

--- packages/grpc-js-core/src/make-client.ts
+++ packages/grpc-js-core/src/make-client.ts
@@ -3,10 +3,6 @@ import { Client, UnaryCallback } from "./client";
 import { CallOptions } from "./call-stream";
 import * as _ from 'lodash';
 
-export interface ClientClassOptions {
-  deprecatedArgumentOrder?: boolean;
-}
-
 export interface Serialize<T> {
   (value: T): Buffer;
 }
@@ -45,40 +41,11 @@ function getDefaultValues<T>(metadata?: Metadata, options?: T): {
 }
 
 /**
- * Map with wrappers for each type of requester function to make it use the old
- * argument order with optional arguments after the callback.
- * @access private
- */
-const deprecated_request_wrap = {
-  unary: (makeUnaryRequest: Function) => {
-    return function makeWrappedUnaryRequest<RequestType, ResponseType>(
-        this: Client, argument: RequestType,
-        callback: UnaryCallback<ResponseType>, metadata?: Metadata,
-        options?: CallOptions) {
-      const opt_args = getDefaultValues(metadata, options);
-      return makeUnaryRequest.call(this, argument, opt_args.metadata,
-                                    opt_args.options, callback);
-    };
-  },
-  client_stream: (makeServerStreamRequest: Function) => {
-    return function makeWrappedClientStreamRequest<RequestType, ResponseType>(
-        this: Client, callback: UnaryCallback<ResponseType>,
-        metadata?: Metadata, options?: CallOptions) {
-      const opt_args = getDefaultValues(metadata, options);
-      return makeServerStreamRequest.call(this, opt_args.metadata,
-                                          opt_args.options, callback);
-    };
-  },
-  server_stream: (f: Function) => f,
-  bidi: (f: Function) => f
-};
-
-/**
  * Map with short names for each of the requester maker functions. Used in
  * makeClientConstructor
  * @private
  */
-const requester_funcs = {
+const requesterFuncs = {
   unary: Client.prototype.makeUnaryRequest,
   server_stream: Client.prototype.makeServerStreamRequest,
   client_stream: Client.prototype.makeClientStreamRequest,
@@ -105,7 +72,7 @@ export type ServiceClientConstructor = typeof Client & {
  */
 export function makeClientConstructor(
     methods: ServiceDefinition, serviceName: string,
-    classOptions: ClientClassOptions): ServiceClientConstructor {
+    classOptions?: {}): ServiceClientConstructor {
   if (!classOptions) {
     classOptions = {};
   }
@@ -116,33 +83,29 @@ export function makeClientConstructor(
   }
 
   _.each(methods, (attrs, name) => {
-    let method_type: keyof typeof requester_funcs;
+    let methodType: keyof typeof requesterFuncs;
     // TODO(murgatroid99): Verify that we don't need this anymore
     if (_.startsWith(name, '$')) {
       throw new Error('Method names cannot start with $');
     }
     if (attrs.requestStream) {
       if (attrs.responseStream) {
-        method_type = 'bidi';
+        methodType = 'bidi';
       } else {
-        method_type = 'client_stream';
+        methodType = 'client_stream';
       }
     } else {
       if (attrs.responseStream) {
-        method_type = 'server_stream';
+        methodType = 'server_stream';
       } else {
-        method_type = 'unary';
+        methodType = 'unary';
       }
     }
     const serialize = attrs.requestSerialize;
     const deserialize = attrs.responseDeserialize;
-    const method_func = _.partial(requester_funcs[method_type], attrs.path,
+    const methodFunc = _.partial(requesterFuncs[methodType], attrs.path,
                                 serialize, deserialize);
-    if (classOptions.deprecatedArgumentOrder) {
-      ServiceClient.prototype[name] = deprecated_request_wrap[method_type](method_func);
-    } else {
-      ServiceClient.prototype[name] = method_func;
-    }
+    ServiceClient.prototype[name] = methodFunc;
     // Associate all provided attributes with the method
     _.assign(ServiceClient.prototype[name], attrs);
     if (attrs.originalName) {
