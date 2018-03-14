6c1f4bc621cb8810872fbd6b2b010b1d9e869d7f
Kelvin Jin <kelvinjin@google.com>
2018-03-14 13:15:38 -0700

    client lib integration changes

--- test/client-libraries-integration/init.sh
+++ test/client-libraries-integration/init.sh
@@ -3,11 +3,13 @@
 npm install
 
 for dir in $(node -p "require('./repositories.json').join('\n')"); do
+  pushd libs
   if [ ! -d $dir ]; then
     git clone https://github.com/googleapis/$dir
+    pushd $dir
+    npm install
+    popd
   fi
-  pushd $dir
-  npm install
   popd
-  node --require ./use-grpc-js.js $(npm bin)/_mocha --timeout 60000 $dir/system-test/*.js
+  SMOKE_TEST_PROJECT=$GCLOUD_PROJECT node --require ./use-grpc-js.js $(npm bin)/_mocha --timeout 60000 libs/$dir/system-test/*.js
 done
