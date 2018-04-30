5405aa31f46ac25f016bbc8654e87fa685545e7b
murgatroid99 <mlumish@google.com>
2018-04-30 13:04:03 -0700

    Upgrade npm on Node 4

--- run-tests.bat
+++ run-tests.bat
@@ -39,6 +39,9 @@ SET FAILED=0
 for %%v in (4 6 7 8 9) do (
   call nvm install %%v
   call nvm use %%v
+  if "%%v"=="4" (
+    npm install -g npm@5
+  )
   @rem https://github.com/mapbox/node-pre-gyp/issues/362
   call npm install -g node-gyp
   node -e "console.log(process.versions)"
