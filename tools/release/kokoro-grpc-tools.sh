27cb1c1b64740ab48d6fe4adb3ad6d6d7fcf6a99
murgatroid99 <mlumish@google.com>
2019-02-07 14:46:04 -0800

    Fix docker run argument ordering

--- tools/release/kokoro-grpc-tools.sh
+++ tools/release/kokoro-grpc-tools.sh
@@ -7,7 +7,7 @@ OS=$(uname)
 case $OS in
 Linux)
   docker build -t kokoro-native-image tools/release/native
-  docker run -v /var/run/docker.sock:/var/run/docker.sock -v $base_dir:$base_dir kokoro-native-image -e ARTIFACTS_OUT=$base_dir/artifacts $base_dir/packages/grpc-tools/build_binaries.sh
+  docker run -v /var/run/docker.sock:/var/run/docker.sock -v $base_dir:$base_dir -e ARTIFACTS_OUT=$base_dir/artifacts kokoro-native-image $base_dir/packages/grpc-tools/build_binaries.sh
   ;;
 Darwin)
   ARTIFACTS_OUT=$base_dir/artifacts ./packages/grpc-tools/build_binaries.sh
