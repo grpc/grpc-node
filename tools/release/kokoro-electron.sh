385d371f9ae3eea8484495418c26e90e536f2fc4
Nicolas Noble <nicolasnoble@users.noreply.github.com>
2019-12-04 11:00:35 -0800

    Attempting to mutate /bin/sh to stop the flakes.

--- tools/release/kokoro-electron.sh
+++ tools/release/kokoro-electron.sh
@@ -16,6 +16,8 @@
 # Deleting Ruby.
 rm -rf ~/.rvm
 
+ln -sf /bin/bash /bin/sh
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
