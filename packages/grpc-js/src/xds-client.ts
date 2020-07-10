930d40916554dd01f6125eb6e759e3370dc981a2
Michael Lumish <mlumish@google.com>
2020-07-10 08:41:58 -0700

    Fix include paths for loading protos

--- packages/grpc-js/src/xds-client.ts
+++ packages/grpc-js/src/xds-client.ts
@@ -62,11 +62,12 @@ function loadAdsProtos(): Promise<adsTypes.ProtoGrpcType> {
         defaults: true,
         oneofs: true,
         includeDirs: [
-          'deps/envoy-api/',
-          'deps/udpa/',
-          'node_modules/protobufjs/',
-          'deps/googleapis/',
-          'deps/protoc-gen-validate/',
+          // Paths are relative to src/build
+          __dirname + '/../../deps/envoy-api/',
+          __dirname + '/../../deps/udpa/',
+          __dirname + '/../../node_modules/protobufjs/',
+          __dirname + '/../../deps/googleapis/',
+          __dirname + '/../../deps/protoc-gen-validate/',
         ],
       }
     )
