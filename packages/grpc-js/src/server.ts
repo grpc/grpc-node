bf41085a7084e2974db0f0b699b435bc192108a8
Slavo Vojacek <public@slavovojacek.com>
2020-09-03 08:24:22 +0100

    fix(grpc-js): Add support for typed add service

--- packages/grpc-js/src/server.ts
+++ packages/grpc-js/src/server.ts
@@ -144,9 +144,13 @@ export class Server {
     throw new Error('Not implemented. Use addService() instead');
   }
 
-  addService(
-    service: ServiceDefinition,
-    implementation: UntypedServiceImplementation
+  addService<
+    ImplementationType extends {
+      [key: string]: any
+    }
+  >(
+    service: ServiceDefinition<Partial<ImplementationType>>,
+    implementation: ImplementationType
   ): void {
     if (this.started === true) {
       throw new Error("Can't add a service to a started server.");
