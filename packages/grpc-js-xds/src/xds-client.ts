b85c70839f4494371d586d463cd2f877c448d0d9
Michael Lumish <mlumish@google.com>
2021-03-04 11:50:30 -0800

    Add more detailed LRS tracing

--- packages/grpc-js-xds/src/xds-client.ts
+++ packages/grpc-js-xds/src/xds-client.ts
@@ -1068,7 +1068,6 @@ export class XdsClient {
     if (!this.lrsCall) {
       return;
     }
-    trace('Sending LRS stats');
     const clusterStats: ClusterStats[] = [];
     for (const [
       { clusterName, edsServiceName },
@@ -1129,6 +1128,7 @@ export class XdsClient {
         }
       }
     }
+    trace('Sending LRS stats ' + JSON.stringify(clusterStats, undefined, 2));
     this.lrsCall.write({
       node: this.lrsNode!,
       cluster_stats: clusterStats,
