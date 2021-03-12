2b0ebcfc6a136e761061cfe357960104888f2402
Michael Lumish <mlumish@google.com>
2021-03-12 14:59:01 -0800

    grpc-js-xds: Add functionality to the xDS interop client

---
+++ packages/grpc-js-xds/interop/generated/grpc/testing/ClientConfigureResponse.ts
@@ -0,0 +1,14 @@
+// Original file: proto/grpc/testing/messages.proto
+
+
+/**
+ * Response for updating a test client's configuration.
+ */
+export interface ClientConfigureResponse {
+}
+
+/**
+ * Response for updating a test client's configuration.
+ */
+export interface ClientConfigureResponse__Output {
+}
