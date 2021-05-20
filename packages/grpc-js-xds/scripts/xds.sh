ec7c819181bf24770e010e81378293dea6d799e5
Michael Lumish <mlumish@google.com>
2021-05-20 10:30:28 -0700

    grpc-js-xds: Enable circuit breaking test

--- packages/grpc-js-xds/scripts/xds.sh
+++ packages/grpc-js-xds/scripts/xds.sh
@@ -52,7 +52,7 @@ GRPC_NODE_TRACE=xds_client,xds_resolver,cds_balancer,eds_balancer,priority,weigh
   GRPC_NODE_VERBOSITY=DEBUG \
   NODE_XDS_INTEROP_VERBOSITY=1 \
   python3 grpc/tools/run_tests/run_xds_tests.py \
-    --test_case="all,timeout" \
+    --test_case="all,timeout,circuit_breaking" \
     --project_id=grpc-testing \
     --source_image=projects/grpc-testing/global/images/xds-test-server-4 \
     --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
