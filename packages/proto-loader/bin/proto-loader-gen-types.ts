grpc/grpc-node

commit f289c343b3393aad73795c0657415f4160bcdcb5

Avoid unused definition imports from proto-loader
Since proto files don't always contain all types of definition, it was
possible to get into a state where generated code contained unused
imports which caused TS errors. This change makes those imports
conditional on the existence of the corresponding definitions in the
proto file.

Co-authored-by: Austin Puri <austin.puri@gmail.com>
Co-authored-by: Joe Porpeglia <josephp@spotify.com>
Signed-off-by: Mike Lewis <mtlewis@users.noreply.github.com>

packages/proto-loader/bin/proto-loader-gen-types.ts
@@ -579,6 +579,34 @@ function generateServiceInterfaces(formatter: TextFormatter, serviceType: Protob
   generateServiceDefinitionInterface(formatter, serviceType);
 }
 
+function containsDefinition(definitionType: typeof Protobuf.Type | typeof Protobuf.Enum, namespace: Protobuf.NamespaceBase): boolean {
+  for (const nested of namespace.nestedArray.sort(compareName)) {
+    if (nested instanceof definitionType) {
+      return true;
+    } else if (isNamespaceBase(nested) && !(nested instanceof Protobuf.Type) && !(nested instanceof Protobuf.Enum)) {
+      return containsDefinition(definitionType, nested);
+    }
+  }
+
+  return false;
+}
+
+function generateDefinitionImports(formatter: TextFormatter, namespace: Protobuf.NamespaceBase, options: GeneratorOptions) {
+  const imports = [];
+
+  if (containsDefinition(Protobuf.Enum, namespace)) {
+    imports.push('EnumTypeDefinition');
+  }
+
+  if (containsDefinition(Protobuf.Type, namespace)) {
+    imports.push('MessageTypeDefinition');
+  }
+
+  if (imports.length) {
+    formatter.writeLine(`import type { ${imports.join(', ')} } from '@grpc/proto-loader';`);
+  }
+}
+
 function generateServiceImports(formatter: TextFormatter, namespace: Protobuf.NamespaceBase, options: GeneratorOptions) {
   for (const nested of namespace.nestedArray.sort(compareName)) {
     if (nested instanceof Protobuf.Service) {
@@ -617,7 +645,7 @@ function generateLoadedDefinitionTypes(formatter: TextFormatter, namespace: Prot
 
 function generateRootFile(formatter: TextFormatter, root: Protobuf.Root, options: GeneratorOptions) {
   formatter.writeLine(`import type * as grpc from '${options.grpcLib}';`);
-  formatter.writeLine("import type { ServiceDefinition, EnumTypeDefinition, MessageTypeDefinition } from '@grpc/proto-loader';");
+  generateDefinitionImports(formatter, root, options);
   formatter.writeLine('');
 
   generateServiceImports(formatter, root, options);

commit 41e09f7d12b05d74f37e3fd30bf08a7398ad12d2

Prevent early return in proto-loader containsDefinition
f289c343b3393aad73795c0657415f4160bcdcb5 introduced a bug - the
recursive for-loop descended into the first elements nested array
and returned that value without iterating over the other members
of the array. This means that the code would only work correctly
when the protofile contained a definition whose name was alphabetically
first amongst its siblings.

This commit fixes the issue by moving the call to containsDefinition
into the if statement to allow iteration to continue if
containsDefinition returns false.

packages/proto-loader/bin/proto-loader-gen-types.ts
@@ -583,8 +583,8 @@ function containsDefinition(definitionType: typeof Protobuf.Type | typeof Protob
   for (const nested of namespace.nestedArray.sort(compareName)) {
     if (nested instanceof definitionType) {
       return true;
-    } else if (isNamespaceBase(nested) && !(nested instanceof Protobuf.Type) && !(nested instanceof Protobuf.Enum)) {
-      return containsDefinition(definitionType, nested);
+    } else if (isNamespaceBase(nested) && !(nested instanceof Protobuf.Type) && !(nested instanceof Protobuf.Enum) && containsDefinition(definitionType, nested)) {
+      return true;
     }
   }
 