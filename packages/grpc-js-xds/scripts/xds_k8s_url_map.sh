5a039aa90b743919782cd64d349830ce46162f82
Michael Lumish <mlumish@google.com>
2022-06-24 15:29:52 -0700

    Use quotation marks for variable substitution
    Co-authored-by: Sergii Tkachenko <hi@sergii.org>

--- packages/grpc-js-xds/scripts/xds_k8s_url_map.sh
+++ packages/grpc-js-xds/scripts/xds_k8s_url_map.sh
@@ -40,7 +40,7 @@ build_test_app_docker_images() {
 
   pushd "${SRC_DIR}"
   docker build \
-    -f ${BUILD_APP_PATH} \
+    -f "${BUILD_APP_PATH}" \
     -t "${CLIENT_IMAGE_NAME}:${GIT_COMMIT}" \
     .
 
