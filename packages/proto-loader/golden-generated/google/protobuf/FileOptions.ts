c7125fbdb5590511c4b88aa5d939e6904db6a283
install <1994052+install@users.noreply.github.com>
2022-11-15 16:44:52 -0500

    proto-loader-gen-types Avoid TS enums

--- packages/proto-loader/golden-generated/google/protobuf/FileOptions.ts
+++ packages/proto-loader/golden-generated/google/protobuf/FileOptions.ts
@@ -4,16 +4,26 @@ import type { IUninterpretedOption as I_google_protobuf_UninterpretedOption, OUn
 
 // Original file: null
 
-export enum _google_protobuf_FileOptions_OptimizeMode {
-  SPEED = 1,
-  CODE_SIZE = 2,
-  LITE_RUNTIME = 3,
-}
+export const _google_protobuf_FileOptions_OptimizeMode = {
+  SPEED: 'SPEED',
+  CODE_SIZE: 'CODE_SIZE',
+  LITE_RUNTIME: 'LITE_RUNTIME',
+} as const;
+
+export type I_google_protobuf_FileOptions_OptimizeMode =
+  | 'SPEED'
+  | 1
+  | 'CODE_SIZE'
+  | 2
+  | 'LITE_RUNTIME'
+  | 3
+
+export type O_google_protobuf_FileOptions_OptimizeMode = typeof _google_protobuf_FileOptions_OptimizeMode[keyof typeof _google_protobuf_FileOptions_OptimizeMode]
 
 export interface IFileOptions {
   'javaPackage'?: (string);
   'javaOuterClassname'?: (string);
-  'optimizeFor'?: (_google_protobuf_FileOptions_OptimizeMode | keyof typeof _google_protobuf_FileOptions_OptimizeMode);
+  'optimizeFor'?: (I_google_protobuf_FileOptions_OptimizeMode | null);
   'javaMultipleFiles'?: (boolean);
   'goPackage'?: (string);
   'ccGenericServices'?: (boolean);
@@ -31,7 +41,7 @@ export interface IFileOptions {
 export interface OFileOptions {
   'javaPackage': (string);
   'javaOuterClassname': (string);
-  'optimizeFor': (keyof typeof _google_protobuf_FileOptions_OptimizeMode);
+  'optimizeFor': (O_google_protobuf_FileOptions_OptimizeMode | null);
   'javaMultipleFiles': (boolean);
   'goPackage': (string);
   'ccGenericServices': (boolean);
