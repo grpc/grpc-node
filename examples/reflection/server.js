6a88cf21f47eb7e4b011a91edeb1a13dc2988af9
Justin Timmons <justinmtimmons@gmail.com>
2023-11-28 22:04:24 -0500

    docs(grpc-reflection): added helloworld implementation for reflection example

--- examples/reflection/server.js
+++ examples/reflection/server.js
@@ -7,8 +7,13 @@ var PROTO_PATH = path.join(__dirname, '../protos/helloworld.proto');
 
 var server = new grpc.Server();
 var packageDefinition = protoLoader.loadSync(PROTO_PATH);
+var proto = grpc.loadPackageDefinition(packageDefinition);
 var reflection = new reflection.ReflectionService(packageDefinition);
+
 reflection.addToServer(server);
+server.addService(proto.helloworld.Greeter.service, {
+  sayHello: (call, callback) => { callback(null, { message: 'Hello' }) }
+});
 
 server.bindAsync('localhost:5000', grpc.ServerCredentials.createInsecure(), () => {
   server.start();
