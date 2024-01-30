2725ea23f91ac2cde989eb02a82bc228bb2107d3
Sergey Saltykov <mishimastar3000@gmail.com>
2024-01-31 01:10:29 +0700

    feat: ability to directly write the gRPC frame to the ServerWritableStream

--- packages/grpc-js/src/server-call.ts
+++ packages/grpc-js/src/server-call.ts
@@ -90,6 +90,16 @@ export type ServerSurfaceCall = {
   getPath(): string;
 } & EventEmitter;
 
+export type ServerWritableBuffer<ResponseType> = {
+  _write(
+    chunk: ResponseType | Buffer,
+    encoding: string,
+    callback: Function
+  ): void;
+  write(chunk: ResponseType | Buffer, cb?: Function): boolean;
+  write(chunk: ResponseType | Buffer, encoding?: any, cb?: Function): boolean;
+};
+
 export type ServerUnaryCall<RequestType, ResponseType> = ServerSurfaceCall & {
   request: RequestType;
 };
@@ -97,14 +107,15 @@ export type ServerReadableStream<RequestType, ResponseType> =
   ServerSurfaceCall & ObjectReadable<RequestType>;
 export type ServerWritableStream<RequestType, ResponseType> =
   ServerSurfaceCall &
-    ObjectWritable<ResponseType> & {
+    ObjectWritable<ResponseType> &
+    ServerWritableBuffer<ResponseType> & {
       request: RequestType;
       end: (metadata?: Metadata) => void;
     };
 export type ServerDuplexStream<RequestType, ResponseType> = ServerSurfaceCall &
   ObjectReadable<RequestType> &
-  ObjectWritable<ResponseType> & { end: (metadata?: Metadata) => void };
-
+  ObjectWritable<ResponseType> &
+  ServerWritableBuffer<ResponseType> & { end: (metadata?: Metadata) => void };
 export class ServerUnaryCallImpl<RequestType, ResponseType>
   extends EventEmitter
   implements ServerUnaryCall<RequestType, ResponseType>
@@ -222,7 +233,7 @@ export class ServerWritableStreamImpl<RequestType, ResponseType>
   }
 
   _write(
-    chunk: ResponseType,
+    chunk: ResponseType | Buffer,
     encoding: string,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     callback: (...args: any[]) => void
@@ -654,15 +665,39 @@ export class Http2ServerCallStream<
     }
   }
 
-  serializeMessage(value: ResponseType) {
-    const messageBuffer = this.handler.serialize(value);
-
+  serializeMessage(value: ResponseType | Buffer) {
     // TODO(cjihrig): Call compression aware serializeMessage().
-    const byteLength = messageBuffer.byteLength;
+
+    if (Buffer.isBuffer(value)) {
+      return this.serializeMessageHandleBuffer(value);
+    }
+
+    const messageBuffer = this.handler.serialize(value);
+    return this.addCompressionAndLength(messageBuffer);
+  }
+
+  private serializeMessageHandleBuffer(value: Buffer): Buffer {
+    const byteLength = value.byteLength;
+    // checking if this is a protobuf message or a gRPC frame
+    if (
+      byteLength >= 5 &&
+      (value.readUInt8(0) === 0 || value.readUint8(0) === 1) &&
+      value.readUInt32BE(1) === byteLength - 5
+    ) {
+      return value;
+    }
+
+    return this.addCompressionAndLength(value);
+  }
+
+  private addCompressionAndLength(value: Buffer, compressed = false) {
+    const byteLength = value.byteLength;
+    const compressionByte = compressed ? 1 : 0;
     const output = Buffer.allocUnsafe(byteLength + 5);
     output.writeUInt8(0, 0);
+    output.writeUInt8(compressionByte, 0);
     output.writeUInt32BE(byteLength, 1);
-    messageBuffer.copy(output, 5);
+    value.copy(output, 5);
     return output;
   }
 
