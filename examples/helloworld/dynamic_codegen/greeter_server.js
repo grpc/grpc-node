9c3291eeaa646d4f2c02ea0fae9004b9c60eb77e
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2024-02-12 14:48:05 +0100

    Fix deprecated code

--- examples/helloworld/dynamic_codegen/greeter_server.js
+++ examples/helloworld/dynamic_codegen/greeter_server.js
@@ -45,7 +45,7 @@ function main() {
   var server = new grpc.Server();
   server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
   server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-    server.start();
+    server;
   });
 }
 
