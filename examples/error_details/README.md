dfcc2f2bd3facc8653fde92d11e0b714e1c9f5ec
Niraj Nepal <nnepal@google.com>
2025-11-14 16:22:21 +0000

    Added error status details example

---
+++ examples/error_details/README.md
@@ -0,0 +1,29 @@
+# Error Details
+
+This example demonstrates the use of status details in grpc errors.
+
+## Start the server
+
+Run the server, which sends a rich error if the name field is empty.
+
+```
+node server.js
+```
+
+## Run the client
+
+Run the client in another terminal. It will make two calls: first, a successful call with a valid name, and second, a failing call with an empty name.
+
+```
+node client.js
+```
+
+## EWxpected Output
+```
+Greeting: Hello World
+
+--- Standard gRPC Error Received ---
+Code: 3
+Status: INVALID_ARGUMENT
+Message: 3 INVALID_ARGUMENT: Simple Error: The name field was empty.
+```
---
+++ examples/error_details/client.js
@@ -0,0 +1,57 @@
+/*
+ *
+ * Copyright 2025 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+var PROTO_PATH = __dirname + '/../protos/helloworld.proto';
+
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+var client = new hello_proto.Greeter('localhost:50051', grpc.credentials.createInsecure());
+
+function main() {
+  client.sayHello({ name: 'World' }, function (err, response) {
+    if (err) {
+      console.error('Success call failed:', err);
+      return;
+    }
+    console.log('Greeting:', response.message);
+
+    client.sayHello({ name: '' }, function (err, response) {
+      if (err) {
+        console.log('--- Standard gRPC Error Received ---');
+        console.log(`Code: ${err.code}`);
+        console.log(`Status: ${grpc.status[err.code]}`);
+        console.log(`Message: ${err.message}`);
+      } else {
+        console.log('Failing call unexpectedly succeeded:', response.message);
+      }
+    });
+  });
+}
+
+main();
